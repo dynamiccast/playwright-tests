ffe9f095d2040eb500e590adba5c5afdac9f80c3
Olivier Berthonneau <olivier.berthonneau@pm.me>
2021-10-29 14:13:33 -0500

    Create testTemplate.js

---
+++ .github/workflows/testTemplate.js
@@ -0,0 +1,8 @@
+const { assertElement, assertNotElement, assertNotText, assertText, faker, logInToFacebook } = require("./helpers");
+
+(async () => {
+
+/* Insert test here */
+
+  process.exit();
+});
